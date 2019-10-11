11700d72fc4ec1ebd985b955d38801439f57ab6f
Emil Grabowski <43807341+Emil9@users.noreply.github.com>
2019-10-11 16:14:26 +0200

    Add files via upload

---
+++ Czytanie z pliku i zapis do pliku/Zad.10.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zad._10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] c = new int[(int)char.MaxValue];
+            string tekst = File.ReadAllText(@"c:\tmp\opowiadanie.txt");
+            foreach (char znak in tekst)
+            {
+                c[(int)znak]++;
+            }
+            string nazwa = @"c:\tmp\znaki.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            System.IO.StreamWriter zapis = new StreamWriter(plik);
+            zapis.Write("znak:częstotliwość   ");
+            for (i = 0; i < (int)char.MaxValue; i++)
+            {
+                if (c[i] > 0 && char.IsLetterOrDigit((char)i))
+                {
+                    zapis.Write("{0}:{1}   ", (char)i, c[i]);
+                }
+            }
+            zapis.Close();
+            plik.Close();
+        }
+    }
+}
